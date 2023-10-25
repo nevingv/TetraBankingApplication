87e545bceb32c1e100ffeee13ed1c611a2e1286e
nevingv <nevingeorge3d@gmail.com>
2023-10-25 16:15:21 -0400

    Add files via upload

---
+++ src/main/java/com/tetra/banking/tetraBankingApplication/TetraBankingApplication.java
@@ -0,0 +1,13 @@
+package com.tetra.banking.tetraBankingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetraBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetraBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tetra/banking/tetraBankingApplication/TetraBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetra.banking.tetraBankingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetraBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
